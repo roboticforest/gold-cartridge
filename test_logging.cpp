f5bfd325a88d51f1a6207df28e0dc1433df88558
Robotic Forest <roboticforest@gmail.com>
2023-06-17 19:32:05 -0700

    Improve clarity of code and usage.

--- test_logging.cpp
+++ test_logging.cpp
@@ -6,22 +6,22 @@
  * A utility function for logging a more meaningful error message ahead of SDL's built-in error descriptions.
  * @param message A short description of program error that occurred.
  */
-void log_error(const std::string &message) {
+[[maybe_unused]] void log_SDL2_error(const std::string& message) {
     std::cerr << message << std::endl;
     std::cerr << "SDL2 last reported: " << SDL_GetError() << std::endl;
 }
 
 /**
  * Dumps information about the given renderer to the console for debugging purposes.
- * @param sdl_renderer The renderer to extract properties from.
- * @param label A short human-readable description of the render.
+ * @param renderer The renderer to extract properties from.
+ * @param renderer_name A short meaningful label of the render. For example: "Main Renderer" or "Texture Render".
  */
-[[maybe_unused]] void dump_render_info(SDL_Renderer *sdl_renderer, const std::string &label) {
+[[maybe_unused]] void dump_render_info(SDL_Renderer* renderer, const std::string& renderer_name) {
 
     SDL_RendererInfo renderer_details;
-    SDL_GetRendererInfo(sdl_renderer, &renderer_details);
+    SDL_GetRendererInfo(renderer, &renderer_details);
 
-    std::cout << label << std::endl;
+    std::cout << renderer_name << std::endl;
     std::cout << "Renderer Details:" << std::endl;
     std::cout << "Name: " << renderer_details.name << std::endl;
     std::cout << "Max Width: " << renderer_details.max_texture_width << std::endl;
--- test_logging.h
+++ test_logging.h
@@ -4,8 +4,8 @@
 #include <string>
 #include <SDL.h>
 
-[[maybe_unused]] void log_error(const std::string& message);
+[[maybe_unused]] void log_SDL2_error(const std::string& message);
 
-[[maybe_unused]] void dump_render_info(SDL_Renderer* sdl_renderer, const std::string& label);
+[[maybe_unused]] void dump_render_info(SDL_Renderer* renderer, const std::string& renderer_name);
 
 #endif //GOLD_CARTRIDGE_TEST_LOGGING_H
