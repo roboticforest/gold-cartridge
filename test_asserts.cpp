a7cf1149c3f859cdf61afc700f18ce2fc30aceb4
Robotic Forest <roboticforest@gmail.com>
2023-06-29 17:12:13 -0700

    Switch to logging tools instead of std::cout.

--- test_asserts.cpp
+++ test_asserts.cpp
@@ -1,13 +1,12 @@
 #include "test_asserts.h"
 
-#include <iostream>
 #include "test_logging.h"
 
 [[maybe_unused]] void assert_task(const std::function<bool()>& task,
                                   const std::optional<std::string>& start_message,
                                   const std::optional<std::string>& failure_message) {
     if (start_message) {
-        std::cout << *start_message << std::endl;
+        log_status(*start_message);
     }
     if (!task()) {
         if (failure_message) {
