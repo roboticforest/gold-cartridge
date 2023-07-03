cd39530401003e6a8d63c21ed6f8c0166a1d48e5
Robotic Forest <roboticforest@gmail.com>
2023-07-03 12:06:20 -0700

    Add support for web colors.

---
+++ colors.h
@@ -0,0 +1,174 @@
+/**
+ * @author David Vitez (AKA: Robotic Forest)
+ * @copyright All rights reserved © 2023 David Vitez
+ * @license This Source Code Form is subject to the terms of the Mozilla Public
+ *          License, v. 2.0. If a copy of the MPL was not distributed with this
+ *          file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef GOLD_CARTRIDGE_COLORS_H
+#define GOLD_CARTRIDGE_COLORS_H
+
+#include <SDL.h>
+
+/**
+ * @brief A collection of all 148 standard web colors.
+ *
+ * For more information visit \n
+ * https://www.w3schools.com/colors/colors_names.asp \n
+ * or search online for "web colors".
+ */
+namespace Color {
+
+    [[maybe_unused]] extern const SDL_Color aliceBlue;
+    [[maybe_unused]] extern const SDL_Color antiqueWhite;
+    [[maybe_unused]] extern const SDL_Color aqua;
+    [[maybe_unused]] extern const SDL_Color aquamarine;
+    [[maybe_unused]] extern const SDL_Color azure;
+    [[maybe_unused]] extern const SDL_Color beige;
+    [[maybe_unused]] extern const SDL_Color bisque;
+    [[maybe_unused]] extern const SDL_Color black;
+    [[maybe_unused]] extern const SDL_Color blanchedAlmond;
+    [[maybe_unused]] extern const SDL_Color blue;
+    [[maybe_unused]] extern const SDL_Color blueViolet;
+    [[maybe_unused]] extern const SDL_Color brown;
+    [[maybe_unused]] extern const SDL_Color burlyWood;
+    [[maybe_unused]] extern const SDL_Color cadetBlue;
+    [[maybe_unused]] extern const SDL_Color chartreuse;
+    [[maybe_unused]] extern const SDL_Color chocolate;
+    [[maybe_unused]] extern const SDL_Color coral;
+    [[maybe_unused]] extern const SDL_Color cornflowerBlue;
+    [[maybe_unused]] extern const SDL_Color cornsilk;
+    [[maybe_unused]] extern const SDL_Color crimson;
+    [[maybe_unused]] extern const SDL_Color cyan;
+    [[maybe_unused]] extern const SDL_Color darkBlue;
+    [[maybe_unused]] extern const SDL_Color darkCyan;
+    [[maybe_unused]] extern const SDL_Color darkGoldenRod;
+    [[maybe_unused]] extern const SDL_Color darkGray;
+    [[maybe_unused]] extern const SDL_Color darkGreen;
+    [[maybe_unused]] extern const SDL_Color darkGrey;
+    [[maybe_unused]] extern const SDL_Color darkKhaki;
+    [[maybe_unused]] extern const SDL_Color darkMagenta;
+    [[maybe_unused]] extern const SDL_Color darkOliveGreen;
+    [[maybe_unused]] extern const SDL_Color darkOrange;
+    [[maybe_unused]] extern const SDL_Color darkOrchid;
+    [[maybe_unused]] extern const SDL_Color darkRed;
+    [[maybe_unused]] extern const SDL_Color darkSalmon;
+    [[maybe_unused]] extern const SDL_Color darkSeaGreen;
+    [[maybe_unused]] extern const SDL_Color darkSlateBlue;
+    [[maybe_unused]] extern const SDL_Color darkSlateGray;
+    [[maybe_unused]] extern const SDL_Color darkSlateGrey;
+    [[maybe_unused]] extern const SDL_Color darkTurquoise;
+    [[maybe_unused]] extern const SDL_Color darkViolet;
+    [[maybe_unused]] extern const SDL_Color deepPink;
+    [[maybe_unused]] extern const SDL_Color deepSkyBlue;
+    [[maybe_unused]] extern const SDL_Color dimGray;
+    [[maybe_unused]] extern const SDL_Color dimGrey;
+    [[maybe_unused]] extern const SDL_Color dodgerBlue;
+    [[maybe_unused]] extern const SDL_Color firebrick;
+    [[maybe_unused]] extern const SDL_Color floralWhite;
+    [[maybe_unused]] extern const SDL_Color forestGreen;
+    [[maybe_unused]] extern const SDL_Color fuchsia;
+    [[maybe_unused]] extern const SDL_Color gainsboro;
+    [[maybe_unused]] extern const SDL_Color ghostWhite;
+    [[maybe_unused]] extern const SDL_Color gold;
+    [[maybe_unused]] extern const SDL_Color goldenRod;
+    [[maybe_unused]] extern const SDL_Color gray;
+    [[maybe_unused]] extern const SDL_Color green;
+    [[maybe_unused]] extern const SDL_Color greenYellow;
+    [[maybe_unused]] extern const SDL_Color grey;
+    [[maybe_unused]] extern const SDL_Color honeydew;
+    [[maybe_unused]] extern const SDL_Color hotPink;
+    [[maybe_unused]] extern const SDL_Color indianRed;
+    [[maybe_unused]] extern const SDL_Color indigo;
+    [[maybe_unused]] extern const SDL_Color ivory;
+    [[maybe_unused]] extern const SDL_Color khaki;
+    [[maybe_unused]] extern const SDL_Color lavender;
+    [[maybe_unused]] extern const SDL_Color lavenderBlush;
+    [[maybe_unused]] extern const SDL_Color lawnGreen;
+    [[maybe_unused]] extern const SDL_Color lemonChiffon;
+    [[maybe_unused]] extern const SDL_Color lightBlue;
+    [[maybe_unused]] extern const SDL_Color lightCoral;
+    [[maybe_unused]] extern const SDL_Color lightCyan;
+    [[maybe_unused]] extern const SDL_Color lightGoldenRodYellow;
+    [[maybe_unused]] extern const SDL_Color lightGray;
+    [[maybe_unused]] extern const SDL_Color lightGreen;
+    [[maybe_unused]] extern const SDL_Color lightGrey;
+    [[maybe_unused]] extern const SDL_Color lightPink;
+    [[maybe_unused]] extern const SDL_Color lightSalmon;
+    [[maybe_unused]] extern const SDL_Color lightSeaGreen;
+    [[maybe_unused]] extern const SDL_Color lightSkyBlue;
+    [[maybe_unused]] extern const SDL_Color lightSlateGray;
+    [[maybe_unused]] extern const SDL_Color lightSlateGrey;
+    [[maybe_unused]] extern const SDL_Color lightSteelBlue;
+    [[maybe_unused]] extern const SDL_Color lightYellow;
+    [[maybe_unused]] extern const SDL_Color lime;
+    [[maybe_unused]] extern const SDL_Color limeGreen;
+    [[maybe_unused]] extern const SDL_Color linen;
+    [[maybe_unused]] extern const SDL_Color magenta;
+    [[maybe_unused]] extern const SDL_Color maroon;
+    [[maybe_unused]] extern const SDL_Color mediumAquamarine;
+    [[maybe_unused]] extern const SDL_Color mediumBlue;
+    [[maybe_unused]] extern const SDL_Color mediumOrchid;
+    [[maybe_unused]] extern const SDL_Color mediumPurple;
+    [[maybe_unused]] extern const SDL_Color mediumSeaGreen;
+    [[maybe_unused]] extern const SDL_Color mediumSlateBlue;
+    [[maybe_unused]] extern const SDL_Color mediumSpringGreen;
+    [[maybe_unused]] extern const SDL_Color mediumTurquoise;
+    [[maybe_unused]] extern const SDL_Color mediumVioletRed;
+    [[maybe_unused]] extern const SDL_Color midnightBlue;
+    [[maybe_unused]] extern const SDL_Color mintCream;
+    [[maybe_unused]] extern const SDL_Color mistyRose;
+    [[maybe_unused]] extern const SDL_Color moccasin;
+    [[maybe_unused]] extern const SDL_Color navajoWhite;
+    [[maybe_unused]] extern const SDL_Color navy;
+    [[maybe_unused]] extern const SDL_Color oldLace;
+    [[maybe_unused]] extern const SDL_Color olive;
+    [[maybe_unused]] extern const SDL_Color oliveDrab;
+    [[maybe_unused]] extern const SDL_Color orange;
+    [[maybe_unused]] extern const SDL_Color orangeRed;
+    [[maybe_unused]] extern const SDL_Color orchid;
+    [[maybe_unused]] extern const SDL_Color paleGoldenRod;
+    [[maybe_unused]] extern const SDL_Color paleGreen;
+    [[maybe_unused]] extern const SDL_Color paleTurquoise;
+    [[maybe_unused]] extern const SDL_Color paleVioletRed;
+    [[maybe_unused]] extern const SDL_Color papayaWhip;
+    [[maybe_unused]] extern const SDL_Color peachPuff;
+    [[maybe_unused]] extern const SDL_Color peru;
+    [[maybe_unused]] extern const SDL_Color pink;
+    [[maybe_unused]] extern const SDL_Color plum;
+    [[maybe_unused]] extern const SDL_Color powderBlue;
+    [[maybe_unused]] extern const SDL_Color purple;
+    [[maybe_unused]] extern const SDL_Color rebeccaPurple;
+    [[maybe_unused]] extern const SDL_Color red;
+    [[maybe_unused]] extern const SDL_Color rosyBrown;
+    [[maybe_unused]] extern const SDL_Color royalBlue;
+    [[maybe_unused]] extern const SDL_Color saddleBrown;
+    [[maybe_unused]] extern const SDL_Color salmon;
+    [[maybe_unused]] extern const SDL_Color sandyBrown;
+    [[maybe_unused]] extern const SDL_Color seaGreen;
+    [[maybe_unused]] extern const SDL_Color seaShell;
+    [[maybe_unused]] extern const SDL_Color sienna;
+    [[maybe_unused]] extern const SDL_Color silver;
+    [[maybe_unused]] extern const SDL_Color skyBlue;
+    [[maybe_unused]] extern const SDL_Color slateBlue;
+    [[maybe_unused]] extern const SDL_Color slateGray;
+    [[maybe_unused]] extern const SDL_Color slateGrey;
+    [[maybe_unused]] extern const SDL_Color snow;
+    [[maybe_unused]] extern const SDL_Color springGreen;
+    [[maybe_unused]] extern const SDL_Color steelBlue;
+    [[maybe_unused]] extern const SDL_Color tan;
+    [[maybe_unused]] extern const SDL_Color teal;
+    [[maybe_unused]] extern const SDL_Color thistle;
+    [[maybe_unused]] extern const SDL_Color tomato;
+    [[maybe_unused]] extern const SDL_Color turquoise;
+    [[maybe_unused]] extern const SDL_Color violet;
+    [[maybe_unused]] extern const SDL_Color wheat;
+    [[maybe_unused]] extern const SDL_Color white;
+    [[maybe_unused]] extern const SDL_Color whiteSmoke;
+    [[maybe_unused]] extern const SDL_Color yellow;
+    [[maybe_unused]] extern const SDL_Color yellowGreen;
+
+} // Color
+
+#endif //GOLD_CARTRIDGE_COLORS_H
