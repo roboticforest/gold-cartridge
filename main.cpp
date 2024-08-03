bc95362d2487b7541b41e68377bb1e42f1e4a2ca
Robotic Forest <roboticforest@gmail.com>
2024-08-03 13:14:21 -0500

    Update copyright.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /**
  * @author David Vitez (AKA: Robotic Forest)
- * @copyright All rights reserved © 2023 David Vitez
+ * @copyright All rights reserved © 2023-2024 David Vitez
  * @license This Source Code Form is subject to the terms of the Mozilla Public
  *          License, v. 2.0. If a copy of the MPL was not distributed with this
  *          file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -26,6 +26,5 @@ int main(int num_args, char** args) {
     });
 
     main_window.run();
-
     return 0;
 }
